391496f946dc45c84f596d645d0a659327dad80f
Petar Ivanov <pepincho95@gmail.com>
2015-02-12 17:00:48 +0200

    add decimalToBinary project

---
+++ DecimalToBinary/WindowsApplication1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim a, b As Integer
+        a = CInt(TextBox1.Text)
+
+        Do While a > 0
+            b = a Mod 2
+            Label1.Text = b & Label1.Text
+            a = a \ 2
+        Loop
+
+    End Sub
+End Class
